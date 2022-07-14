509a2f06414df2f51d384f94336c63488071c50b
TonyBallo <acballo77@gmail.com>
2022-07-13 17:19:50 -0700

    signOfArray fix

---
+++ signOfArray.java
@@ -0,0 +1,19 @@
+
+
+public class signOfArray {
+    public int arraySign(int[] nums) {
+        int sign = 1; 
+        
+        for (int num : nums) {
+            if (num == 0) 
+                return 0; 
+            else if (num < 0) 
+                sign = -sign; 
+     
+                
+        }
+        
+        return sign; 
+        
+    }
+}
